5a9380dd672c688540315022fc7e0e62185c7cb8
Konstantin <putivcev123@gmail.com>
2025-08-01 10:24:49 +0300

    main: some fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@ import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/zabor-belgorod',
+  assetPrefix: '/zabor-belgorod/',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(path.dirname('.'), 'src');
     return config;
